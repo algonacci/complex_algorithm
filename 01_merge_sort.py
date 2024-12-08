3aeccac617a4e9495c4a57a3db74ca96f440e642
algonacci <86970816+algonacci@users.noreply.github.com>
2024-12-08 21:12:18 +0700

    Create 01_merge_sort.py

---
+++ 01_merge_sort.py
@@ -0,0 +1,40 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+    
+
+def merge(left, right):
+    result = []
+    left_index, right_index = 0, 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else: 
+            result.append(right[right_index])
+            right_index += 1
+
+    while left_index < len(left):
+        result.append(left[left_index])
+        left_index += 1
+
+    while right_index < len(right):
+        result.append(right[right_index])
+        right_index += 1
+
+    return result
+
+arr = [15, 78, 65, 23, 41, 12, 7, 36]
+print("Before sorted: ", arr)
+sorted_array = merge_sort(arr)
+print("After sorted: ", sorted_array)
